e7344fff0ed089edaacfd5fd85271c3277e7c649
Mukesh Didel <mukeshdidel11111@gmail.com>
2026-06-09 00:59:26 +0530

    backend init

---
+++ backend/AssignmentBotmakers/src/main/java/dev/mukesh/AssignmentBotmakers/AssignmentBotmakersApplication.java
@@ -0,0 +1,13 @@
+package dev.mukesh.AssignmentBotmakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentBotmakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentBotmakersApplication.class, args);
+	}
+
+}
---
+++ backend/AssignmentBotmakers/src/test/java/dev/mukesh/AssignmentBotmakers/AssignmentBotmakersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mukesh.AssignmentBotmakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBotmakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
